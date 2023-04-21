3d374ac8e29e0b9dac63ad68e5d11f8d7cbd0f95
selamalka <99277476+selamalka@users.noreply.github.com>
2023-04-21 09:40:50 +0300

    player is pushed backwards when melee to the sides

--- Assets/Scripts/Player/MeleeSlash.cs
+++ Assets/Scripts/Player/MeleeSlash.cs
@@ -2,11 +2,45 @@ using UnityEngine;
 
 public class MeleeSlash : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 enemyDirection;
+    private bool shouldPush;
+
+    private void Start()
+    {
+       rb = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
+            shouldPush = true;
             collision.GetComponent<IDamageable>().TakeDamage(PlayerStatsManager.Instance.MeleeDamage);
+            enemyDirection = collision.gameObject.transform.position - transform.position;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (shouldPush)
+        {
+            PushPlayer(enemyDirection);
+            shouldPush = false;
+        }
+    }
+
+    private void PushPlayer(Vector2 enemyDirection)
+    {
+        if (enemyDirection.x > 0)
+        {
+            rb.velocity = Vector2.left * 50; 
+            print(rb.velocity.x);
+        }
+        else if (enemyDirection.x < 0)
+        {
+            rb.velocity = Vector2.right * 50;
+            print(rb.velocity.x);
         }
     }
 }
