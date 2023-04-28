229f32817c3ebaf82c1aee478b27845aa51188ac
selamalka <99277476+selamalka@users.noreply.github.com>
2023-04-28 20:21:41 +0300

    still working on spell unlocking

--- Assets/Scripts/Scriptable Objects/SpellData.cs
+++ Assets/Scripts/Scriptable Objects/SpellData.cs
@@ -7,8 +7,15 @@ public class SpellData : ScriptableObject
     [field: SerializeField] public string Name { get; private set; }
     [field: SerializeField] public Sprite Sprite { get; private set; }
     [field: SerializeField] public int Level { get; private set; }
+    [field: SerializeField] public int SpellPointCost { get; private set; }
+    [field: SerializeField] public bool IsSpellUnlocked { get; private set; }
     [field: SerializeField] public float Speed { get; private set; }
     [field: SerializeField] public int ManaCost { get; private set; }
     [field: SerializeField] public int Damage { get; private set; }
-    [field: SerializeField] public float Lifespan { get; private set; }
+    [field: SerializeField] public float Lifespan { get; private set; }   
+
+    public void SetIsSpellUnlocked(bool value)
+    {
+        IsSpellUnlocked = value;
+    }
 }
