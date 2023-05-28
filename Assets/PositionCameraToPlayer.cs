84ad5909f45e5d63de40ecd6fbdaad1eb066db00
selamalka <selamalka@gmail.com>
2023-05-28 12:46:52 +0300

    fixed some camera stuff

--- Assets/PositionCameraToPlayer.cs
+++ Assets/PositionCameraToPlayer.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 
 public class PositionCameraToPlayer : MonoBehaviour
 {
+    private void Awake()
+    {
+        transform.position = new Vector3(115f, -5.6f, -10f);
+    }
+
     private void Start()
     {
         SetCameraToPlayer();
-        transform.position = new Vector3(115f, -5.6f, -10f);
+        if (ProgressionManager.Instance.IsTesting)
+        {
+            transform.position = GameObject.FindWithTag("Player").transform.position + new Vector3(12f, 8.4f, -10f);
+        }
     }
 
     public void SetCameraToPlayer()
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -15,6 +15,9 @@ public class SceneLoader : MonoBehaviour
             await Task.Delay(2000);
             ProgressionManager.Instance.Progression.SetLastCheckpoint(new (103,-14,0));
             ProgressionManager.Instance.Progression.SetSpellPoints(PlayerStatsManager.Instance.SpellPoints);
+            ProgressionManager.Instance.Progression.SetHealthPoints(PlayerStatsManager.Instance.CurrentHealth);
+            ProgressionManager.Instance.Progression.SetManaPoints(PlayerStatsManager.Instance.CurrentMana);
+            ProgressionManager.Instance.Progression.SetSoulPoints(PlayerStatsManager.Instance.CurrentSouls);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         else
