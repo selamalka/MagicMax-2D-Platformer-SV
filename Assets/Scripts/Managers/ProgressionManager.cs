16681c2789d5d7b0895e3abeebd0f167fd9c6b56
selamalka <selamalka@gmail.com>
2023-05-28 13:26:24 +0300

    i think everything that should be saved between scenes is saved. for now.

--- Assets/Scripts/Managers/ProgressionManager.cs
+++ Assets/Scripts/Managers/ProgressionManager.cs
@@ -5,6 +5,7 @@ public class ProgressionManager : MonoBehaviour
     public static ProgressionManager Instance;
     [field: SerializeField] public Progression Progression { get; private set; }
     [field: SerializeField] public bool IsTesting { get; private set; }
+    [field: SerializeField] public bool IsCheckpointSaved { get; private set; }
 
     private void Awake()
     {
@@ -28,17 +29,27 @@ public class ProgressionManager : MonoBehaviour
             Progression.SetCurrentExpPoints(0);
             UIManager.Instance.UpdateExpBar();
 
+            Progression.SetSpellPoints(0);
+
             Progression.UnlockedSpellsList.Clear();
             Progression.SetSpellSlot1Info(null, null);
             Progression.SetSpellSlot2Info(null, null);
             Progression.SetLastCheckpoint(new Vector3(103, -14, 0));
+            IsCheckpointSaved = false;
             Progression.SetIsNewGame(false);
         }
 
         if (!IsTesting)
         {
-            GameObject.Find("Player").transform.position = Progression.LastCheckpoint;
+            GameObject.FindWithTag("Player").transform.position = Progression.LastCheckpoint;
+            GameObject.FindWithTag("CameraContainer").transform.position = Progression.LastCheckpoint + new Vector3(12f, 8.4f, -10f);
         }
+
         FindObjectOfType<PositionCameraToPlayer>().SetCameraToPlayer();
+    }
+
+    public void SetIsCheckpointSaved(bool value)
+    {
+        IsCheckpointSaved = value;
     }
 }
