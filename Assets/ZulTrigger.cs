f947c03194937b53d83af1cf3d3ed528e63fa1f1
selamalka <selamalka@gmail.com>
2023-05-31 13:55:56 +0300

    zul is triggered

---
+++ Assets/ZulTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ZulTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            FindObjectOfType<ZulAI>().SetIsTriggered(true);
+        }
+    }
+}
